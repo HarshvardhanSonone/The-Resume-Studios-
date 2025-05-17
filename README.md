934ada388913de5cdec5c599670621b76eeb3650
HarshvardhanSonone <156497982+HarshvardhanSonone@users.noreply.github.com>
2025-05-17 15:22:15 +0530

    a commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # The Resume Studios 
 Build fully functional web-app to  build/generate CVs easily
+
+facalites 
+
+- Can export CVs in the form of PDF
+- toggle b/w light and dark mode
+-Real time changes are visible
---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+
+// form repeater
+$(document).ready(function () {
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show: function () {
+            $(this).slideDown();
+        },
+        hide: function (deleteElement) {
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+
+
+     $('#darkModeToggle').on('click', function () {
+        $('body').toggleClass('dark-mode');
+    });
+});
+
+
+
+// generate cv
